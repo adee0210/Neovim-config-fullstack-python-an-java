291f2df37c71c08bfccdbb69b6ca5ddc4a34f26a
adee0210 <anhduc02102004@gmail.com>
2025-03-22 01:19:34 +0700

    change theme tokyo night

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,33 +1,29 @@
--- /home/duc/.config/nvim/lua/plugins.lua
 return {
-    -- Plugin Onedark (Colorscheme)
+    -- Plugin Tokyo Night (Colorscheme)
     {
-        "navarasu/onedark.nvim",
+        "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            require("onedark").setup({
-                style = "dark",
-                transparent = true,
-                term_colors = true,
-                ending_tildes = false,
-                cmp_itemkind_reverse = false,
-                code_style = {
-                    comments = "italic",
-                    keywords = "none",
-                    functions = "none",
-                    strings = "none",
-                    variables = "none",
+            require("tokyonight").setup({
+                style = "night", -- Chọn style: "storm", "moon", "night", hoặc "day"
+                transparent = true, -- Bật nền trong suốt cho toàn bộ giao diện
+                terminal_colors = true, -- Áp dụng màu cho terminal
+                styles = {
+                    comments = { italic = true }, -- Kiểu chữ nghiêng cho comments
+                    keywords = { italic = false },
+                    functions = {},
+                    variables = {},
+                    sidebars = "transparent", -- Đặt sidebar (như Vim Tree) trong suốt
+                    floats = "transparent", -- Đặt floating windows trong suốt
                 },
-                diagnostics = {
-                    darker = true,
-                    undercurl = true,
-                    background = true,
-                },
-                colors = {},
-                highlights = {},
+                sidebars = { "qf", "vista_kind", "terminal", "packer", "NvimTree" }, -- Thêm NvimTree vào danh sách sidebar
+                day_brightness = 0.3, -- Độ sáng khi dùng style "day"
+                hide_inactive_statusline = false, -- Ẩn statusline khi không hoạt động
+                dim_inactive = false, -- Làm mờ các cửa sổ không hoạt động
+                lualine_bold = true, -- In đậm cho lualine
             })
-            vim.cmd.colorscheme "onedark"
+            vim.cmd([[colorscheme tokyonight]]) -- Kích hoạt colorscheme
         end,
     },
 
